d5d294219b8b08bdcafe0e6270d55054dcbfe3d8
Nandan Rao <nandanmarkrao@gmail.com>
2018-05-30 21:46:32 +0200

    Fixed no-continue bug in Play

--- src/steps/Play.js
+++ src/steps/Play.js
@@ -12,7 +12,7 @@ export default (props) => {
   const {round, transitioning, button} = props
   const boxes = props.boxes[props.boxes.length - 1]
 
-  const allBoxesClicked = boxes.filter(b => !!b.result).reduce((a,b) => a+1, 0) === boxes.length
+  const allBoxesClicked = getResults(boxes).filter(res => !!res).reduce((a,b) => a+1, 0) === boxes.length
 
   let curr = getResults(boxes).filter(r => !!r).length
   if (transitioning) {
@@ -21,15 +21,13 @@ export default (props) => {
 
   const winnings = transitioning ? 0 : getWinnings(boxes)
 
-  // const playBoxes = boxes.map((b,i) => <Playbox key={i} report={boxPlayed.bind(this, round, i)} result={b.result} outcome={b.outcome}/>)
-
   const playboxes = boxes.map((pairs,i) => {
     return <BoxPair boxes={pairs} idx={i} round={round} key={i}/>
   })
 
   return <div className="play">
     <Rounds boxes={boxes} />
-    {winnings > 0 ? null : <div className="boxes"> {playboxes[curr]} </div>}
+    {winnings > 0 ? null : <div className="boxes"> {playboxes[curr] || playboxes[curr-1]} </div>}
     <h3 className={winnings === 0 ? null : 'won'}> Won: ${winnings} </h3>
     { !button ? null : <button disabled={!allBoxesClicked && winnings === 0} className="accept" onClick={() => store.dispatch(push(`/question?q=${ 1 }`))}> continue </button>}
 
